197bc6d5a956e9bdb92564bb8325c75f94d3d71d
Abraham El Hage <abrahamelhage2003@gmail.com>
2026-05-12 11:33:07 -0400

    changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Text, View } from "react-native";
-// @ts-ignore: side-effect import has no type declarations
-import "../global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../app/global.css";
+// @ts-ignore: side-effect import has no type adeclarations
 
 const RootLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text className="text-3xl bg-blue-100">RootLayout</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
---
+++ core/actions/movies/now-playing.action.ts
@@ -0,0 +1,7 @@
+export const nowPlayingAction = async () => {
+  try {
+  } catch (error) {
+    console.log(error);
+    throw "Cannot load now playing movies";
+  }
+};
