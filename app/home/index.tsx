e4c5eace8b974a9a922c932e55640f1ef63621d2
Abraham El Hage <abrahamelhage2003@gmail.com>
2026-05-13 11:26:27 -0400

    carru

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,13 +1,47 @@
-import { useMovies } from "@/presentation/hooks/useMovies";
 import { Text, View } from "react-native";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
-export default function Index() {
+import MainNowPlayingSlideShow from "@/presentation/components/MainNowPlayingSlideshow";
+import { useMovies } from "@/presentation/hooks/useMovies";
+
+const HomeScreen = () => {
   const { nowPlayingQuery } = useMovies();
+  const safeArea = useSafeAreaInsets();
+
+  if (nowPlayingQuery.isLoading) {
+    console.log("isLoading");
+    return (
+      <SafeAreaView>
+        <View className="mt-2" style={{ paddingTop: safeArea.top }}>
+          <Text>Loading</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+  if (nowPlayingQuery.isFetching) {
+    console.log("isfetching");
+    return (
+      <SafeAreaView>
+        <View className="mt-2" style={{ paddingTop: safeArea.top }}>
+          <Text>Fetching</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
 
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-2" style={{ paddingTop: safeArea.top }}>
+        <Text className="text-3xl">HomeScreen</Text>
+
+        {/*carrusel*/}
+        <MainNowPlayingSlideShow></MainNowPlayingSlideShow>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default HomeScreen;
---
+++ presentation/components/mainNowPlayingSlideshow.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const MainNowPlayingSlideShow = () => {
+  return (
+    <View>
+      <Text>MainNowPlayingSlideShow</Text>
+    </View>
+  );
+};
+
+export default MainNowPlayingSlideShow;
